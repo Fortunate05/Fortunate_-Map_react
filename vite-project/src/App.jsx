20a8c3625e5e2a6538eb46a3cf5178e6c705b77e
unknown <you@example.com>
2023-04-13 13:26:06 +0200

    add new page and install router, and navbar , css create

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,28 +1,31 @@
-
+import './component/Navbar'
 import './App.css'
-import Card from 'react-bootstrap/Card';
+import Navbar from './component/Navbar'
 
 function App() {
 
   const person = [
-    { id: '1', name: 'Fortunate', text: 'she is hardworking'},
+    { id: '1', name: 'Fortunate', text: 'she is hardworking' },
     { id: '2', name: 'Asanda', text: 'she is hardworking' },
-    { id: '3', name: 'Thozama', text:'' },
-    { id: '4', name: 'Siyanda', text:'' },
-    { id: '5', name: 'Sibongile' , text:''},
-    { id: '6', name: 'Reginal', text:'' },
-    { id: '7', name: 'Tshepo', text:'' },
-    { id: '8', name: 'Bonke' , text:''},
-    { id: '9', name: 'Sonke', text:'' },
-    { id: '10', name: 'Bathande', text:'' },
-    { id: '12', name: 'Nkosivumile' , text:''},
-    { id: '13', name: 'Melusi', text:'' },
-    { id: '14', name: 'Thembani', text:'' },
+    { id: '3', name: 'Thozama', text: '' },
+    { id: '4', name: 'Siyanda', text: '' },
+    { id: '5', name: 'Sibongile', text: '' },
+    { id: '6', name: 'Reginal', text: '' },
+    { id: '7', name: 'Tshepo', text: '' },
+    { id: '8', name: 'Bonke', text: '' },
+    { id: '9', name: 'Sonke', text: '' },
+    { id: '10', name: 'Bathande', text: '' },
+    { id: '12', name: 'Nkosivumile', text: '' },
+    { id: '13', name: 'Melusi', text: '' },
+    { id: '14', name: 'Thembani', text: '' },
   ]
 
   return (
     <>
       <div>
+        <Navbar />
+
+
         <h1>Staff PWD 2022</h1>
         <div class="grid-container">
           {
