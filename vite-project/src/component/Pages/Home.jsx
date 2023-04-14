ad4a096427682bd2ab7f2bcd9988dc79817be23b
unknown <you@example.com>
2023-04-14 10:54:15 +0200

    add list for DA what we did

--- vite-project/src/component/Pages/Home.jsx
+++ vite-project/src/component/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../Navbar'
 import ListGroup from 'react-bootstrap/ListGroup';
 
+
 export default function Home() {
   return (
     <div>
@@ -9,15 +10,15 @@ export default function Home() {
 
       
       <ListGroup>
-        <ListGroup.Item>No style</ListGroup.Item>
-        <ListGroup.Item variant="primary">Primary</ListGroup.Item>
-        <ListGroup.Item variant="secondary">Secondary</ListGroup.Item>
-        <ListGroup.Item variant="success">Success</ListGroup.Item>
-        <ListGroup.Item variant="danger">Danger</ListGroup.Item>
-        <ListGroup.Item variant="warning">Warning</ListGroup.Item>
-        <ListGroup.Item variant="info">Info</ListGroup.Item>
-        <ListGroup.Item variant="light">Light</ListGroup.Item>
-        <ListGroup.Item variant="dark">Dark</ListGroup.Item>
+        <ListGroup.Item>Deaf Staff</ListGroup.Item>
+        <ListGroup.Item variant="primary">We here Digital Academy at Braamfontein.</ListGroup.Item>
+        <ListGroup.Item variant="secondary">We study at Information Technology Support System.</ListGroup.Item>
+        <ListGroup.Item variant="success">We learn Code and Logical.</ListGroup.Item>
+        <ListGroup.Item variant="danger">We have a interpret for Deaf staff.</ListGroup.Item>
+        <ListGroup.Item variant="warning">We have a laptops and Wifi for us learn trainning at IT support System.</ListGroup.Item>
+        <ListGroup.Item variant="info">We have a LMS doing every Thursday.</ListGroup.Item>
+        <ListGroup.Item variant="light">We have a good clean environment</ListGroup.Item>
+        <ListGroup.Item variant="dark">We have a presentation as show office how we work</ListGroup.Item>
       </ListGroup>
       
 
