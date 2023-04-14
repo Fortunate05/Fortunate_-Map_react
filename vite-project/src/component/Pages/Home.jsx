492461654eca8955e5f7d8fe4f877c0044eef379
unknown <you@example.com>
2023-04-14 13:49:10 +0200

    . tect in list group

--- vite-project/src/component/Pages/Home.jsx
+++ vite-project/src/component/Pages/Home.jsx
@@ -23,8 +23,8 @@ export default function Home() {
         <ListGroup.Item variant="danger">We have a interpret for Deaf staff.</ListGroup.Item>
         <ListGroup.Item variant="warning">We have a laptops and Wifi for us learn trainning at IT support System.</ListGroup.Item>
         <ListGroup.Item variant="info">We have a LMS doing every Thursday.</ListGroup.Item>
-        <ListGroup.Item variant="light">We have a good clean environment</ListGroup.Item>
-        <ListGroup.Item variant="dark">We have a presentation as show office how we work</ListGroup.Item>
+        <ListGroup.Item variant="light">We have a good clean environment.</ListGroup.Item>
+        <ListGroup.Item variant="dark">We have a presentation as show office how we work.</ListGroup.Item>
       </ListGroup>
 
 
