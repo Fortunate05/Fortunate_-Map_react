8a0867ae7878b3e63f6823e12a1764651517c80b
unknown <you@example.com>
2023-05-16 16:04:00 +0200

    Aboutus change what we did

--- vite-project/src/pages/AboutUs.jsx
+++ vite-project/src/pages/AboutUs.jsx
@@ -9,7 +9,7 @@ function AboutUs() {
     <div>
       <Navbar/>
       <ListGroup>
-        <ListGroup.Item><h3>Deaf Staff</h3><Placeholder as="p" animation="glow">
+        <ListGroup.Item><h3>PWD doing what</h3><Placeholder as="p" animation="glow">
           <Placeholder xs={12} />
         </Placeholder></ListGroup.Item>
         <ListGroup.Item variant="primary">We here Digital Academy at Braamfontein.
@@ -19,7 +19,7 @@ function AboutUs() {
         <ListGroup.Item variant="danger">We have a interpret for Deaf staff.</ListGroup.Item>
         <ListGroup.Item variant="warning">We have a laptops and Wifi for us learn trainning at IT support System.</ListGroup.Item>
         <ListGroup.Item variant="info">We have a LMS doing every Thursday.</ListGroup.Item>
-        <ListGroup.Item variant="light">We have a good clean environment.</ListGroup.Item>
+        <ListGroup.Item variant="light">We have a free bread and Tea.</ListGroup.Item>
         <ListGroup.Item variant="dark">We have a presentation as show office how we work.</ListGroup.Item>
       </ListGroup>
     </div>
