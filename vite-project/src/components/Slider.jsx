dfa1404516d0c1ba8dd048fdef61a047ff2cdfdb
unknown <you@example.com>
2023-05-16 11:49:11 +0200

    add hr and import for picture on slider

--- vite-project/src/components/Slider.jsx
+++ vite-project/src/components/Slider.jsx
@@ -1,10 +1,49 @@
 import React from 'react'
+import { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import '../components/Slider.css'
+import image1 from '../assets/DA_Logo.jpg'
+
 
 export default function Slider() {
+
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
   return (
     <div>
 
-      this is slider
+<Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img
+          className="d-block"
+          src={image1}
+          alt="First slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block"
+          src="holder.js/800x400?text=Second slide&bg=282c34"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block"
+          src="holder.js/800x400?text=Third slide&bg=20232a"
+          alt="Third slide"
+        />
+
+      </Carousel.Item>
+    </Carousel>
 
 
 
