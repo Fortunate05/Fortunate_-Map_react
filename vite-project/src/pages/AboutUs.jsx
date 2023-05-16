f6414d991c4855eab26e5cbbf276417cbb38b375
unknown <you@example.com>
2023-05-16 16:13:57 +0200

    update what we did in aboutus

--- vite-project/src/pages/AboutUs.jsx
+++ vite-project/src/pages/AboutUs.jsx
@@ -19,7 +19,7 @@ function AboutUs() {
         <ListGroup.Item variant="danger">We have a interpret for Deaf staff.</ListGroup.Item>
         <ListGroup.Item variant="warning">We have a laptops and Wifi for us learn trainning at IT support System.</ListGroup.Item>
         <ListGroup.Item variant="info">We have a LMS doing every Thursday.</ListGroup.Item>
-        <ListGroup.Item variant="light">We have a free bread and Tea.</ListGroup.Item>
+        <ListGroup.Item variant="light">We have a skill code programmer.</ListGroup.Item>
         <ListGroup.Item variant="dark">We have a presentation as show office how we work.</ListGroup.Item>
       </ListGroup>
     </div>
