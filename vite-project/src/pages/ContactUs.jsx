70da853db137253e97aba2139858e966ffd7a0b4
unknown <you@example.com>
2023-05-16 13:35:27 +0200

    add hr on contact us

--- vite-project/src/pages/ContactUs.jsx
+++ vite-project/src/pages/ContactUs.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import '../Pages/Home.css'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Placeholder from 'react-bootstrap/Placeholder';
 
 
 
 export default function Home() {
   return (
     <div>
-
       <Navbar/>
-this is contact
+      
 
 
-    </div>
+
+
+      <hr></hr>
+      
+</div>
+    
   )
 }
