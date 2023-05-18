7b62685db65bb7b3c580ef4d31d289eebd191907
unknown <you@example.com>
2023-05-18 09:14:49 +0200

    add picture and change each in DA.JSX

--- vite-project/src/pages/DA.jsx
+++ vite-project/src/pages/DA.jsx
@@ -12,16 +12,16 @@ function DA() {
 
   const person = [
     { id: '1', text: 'a testimonial from Fortunate , who is in our IT System Support',image:'https://media.discordapp.net/attachments/1021684832922779648/1021685478560370728/IMG_20220920_085957.jpg?width=460&height=613'  },
-    { id: '2', text: 'a testimonial from Thozama , who is in our IT System Support',image:'https://cdn.discordapp.com/attachments/1021684832922779648/1021685470616371240/IMG_20220920_085931.jpg' },
-    { id: '3', text: 'a testimonial from Sibongile , who is in our IT System Support' ,image:'https://cdn.discordapp.com/attachments/1021684832922779648/1021685444792041472/IMG_20220920_085751.jpg'},
-    { id: '4', text: 'a testimonial from Reginah , who is in our IT System Support',image:'https://cdn.discordapp.com/attachments/1021684832922779648/1021685547468587079/IMG_20220920_085647.jpg' },
-    { id: '5', text: 'a testimonial from Tshepo , who is in our IT System Support',image:'https://cdn.discordapp.com/attachments/994851596112502837/1044508435259076758/IMG_6848.jpg' },
-    { id: '6', text: 'a testimonial from Bonke , who is in our IT System Support' ,image:'https://cdn.discordapp.com/attachments/1096068491288776896/1096069590561329302/IMG-20230413-WA0003.jpg'},
-    { id: '7', text: 'a testimonial from Sonke , who is in our IT System Support',image:'https://cdn.discordapp.com/attachments/1096058165172391956/1096075408224563220/IMG-20230413-WA0026.jpg' },
-    { id: '8', text: 'a testimonial from Bathande , who is in our IT System Support',image:'https://cdn.discordapp.com/attachments/1096055379567333446/1096055670777851914/18A7E0EE-2168-47A5-A1E6-2070DBF0A52C.jpg' },
-    { id: '9', text: 'a testimonial from Nkosivumile , who is in our IT System Support',image:'https://cdn.discordapp.com/attachments/1096068491288776896/1096069111085281351/IMG-20230413-WA0002.jpg' },
-    { id: '10', text: 'a testimonial from Melusi , who is in our IT System Support' ,image:'https://cdn.discordapp.com/attachments/1096068491288776896/1096068968080482405/Screenshot_20230413_154551.jpg'},
-    { id: '11', text: 'a testimonial from Mbuyiselo , who is in our IT System Support',image:'https://media.discordapp.net/attachments/1096058730480668782/1108020470885912607/IMG_6837.jpg?width=460&height=613' },
+    { id: '2', text: 'a testimonial from Bathande , who is in our IT System Support',image:'https://cdn.discordapp.com/attachments/1096055379567333446/1108322303856607272/5813F856-D149-46DF-85F5-DB6573CE6AD1.jpg' },
+    { id: '3', text: 'a testimonial from Melusi , who is in our IT System Support' ,image:'https://media.discordapp.net/attachments/1096055379567333446/1108321042310627499/IMG_3689.jpg?width=372&height=662'},
+    { id: '4', text: 'a testimonial from Nkosivumile , who is in our IT System Support',image:'https://cdn.discordapp.com/attachments/1096068491288776896/1096069111085281351/IMG-20230413-WA0002.jpg' },
+    { id: '5', text: 'a testimonial from Mbuyiselo , who is in our IT System Support',image:'https://media.discordapp.net/attachments/1096058730480668782/1108020470885912607/IMG_6837.jpg?width=460&height=613' },
+    { id: '6', text: 'a testimonial from Sonke , who is in our IT System Support',image:'https://cdn.discordapp.com/attachments/1096058165172391956/1096075408224563220/IMG-20230413-WA0026.jpg' },
+    { id: '7', text: 'a testimonial from Thozama , who is in our IT System Support',image:'https://cdn.discordapp.com/attachments/1021684832922779648/1021685470616371240/IMG_20220920_085931.jpg' },
+    { id: '8', text: 'a testimonial from Sibongile , who is in our IT System Support' ,image:'https://cdn.discordapp.com/attachments/1021684832922779648/1021685444792041472/IMG_20220920_085751.jpg'},
+    { id: '9', text: 'a testimonial from Reginah , who is in our IT System Support',image:'https://cdn.discordapp.com/attachments/1021684832922779648/1021685547468587079/IMG_20220920_085647.jpg' },
+    { id: '10', text: 'a testimonial from Tshepo , who is in our IT System Support',image:'https://cdn.discordapp.com/attachments/994851596112502837/1044508435259076758/IMG_6848.jpg' },
+    { id: '11', text: 'a testimonial from Bonke , who is in our IT System Support' ,image:'https://cdn.discordapp.com/attachments/1096068491288776896/1096069590561329302/IMG-20230413-WA0003.jpg'},
     { id: '12', text: 'a testimonial from Thembani , who is in our IT System Support',image:'https://cdn.discordapp.com/attachments/1096056243568779284/1096057082073067572/Screenshot_2023-04-13_145726.png ' },
   ]
 
