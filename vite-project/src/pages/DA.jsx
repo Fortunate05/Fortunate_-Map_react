7e8e1dbd8c81bb838786628c24332a2235eab00d
unknown <you@example.com>
2023-05-18 09:50:22 +0200

    add each student

--- vite-project/src/pages/DA.jsx
+++ vite-project/src/pages/DA.jsx
@@ -11,12 +11,12 @@ function DA() {
   ]
 
   const person = [
-    { id: '1', text: 'a testimonial from Fortunate , Thank you for the learning experience.',image:'https://media.discordapp.net/attachments/1021684832922779648/1021685478560370728/IMG_20220920_085957.jpg?width=460&height=613'  },
+    { id: '1', text: 'a testimonial from Fortunate ,Thank you for reaching out about Level 5 on your team DA, i appreciate you motivate us to hard working and learn code HTML,CSS and ReactJs.',image:'https://media.discordapp.net/attachments/1021684832922779648/1021685478560370728/IMG_20220920_085957.jpg?width=460&height=613'  },
     { id: '2', text: 'a testimonial from Bathande ,  i really appreciate the opportunity for Level 5 i learnt enjoy.',image:'https://cdn.discordapp.com/attachments/1096055379567333446/1108322303856607272/5813F856-D149-46DF-85F5-DB6573CE6AD1.jpg' },
     { id: '3', text: 'a testimonial from Melusi , Thank you for opportunity how teach me confidence, learn skill.' ,image:'https://media.discordapp.net/attachments/1096055379567333446/1108321042310627499/IMG_3689.jpg?width=372&height=662'},
     { id: '4', text: 'a testimonial from Nkosivumile , Thank you for providing me with the opportunity to work with you and your team.',image:'https://cdn.discordapp.com/attachments/1096068491288776896/1096069111085281351/IMG-20230413-WA0002.jpg' },
     { id: '5', text: 'a testimonial from Mbuyiselo , Thank you for the opportunity to learn from Digital Academy',image:'https://media.discordapp.net/attachments/1096058730480668782/1108020470885912607/IMG_6837.jpg?width=460&height=613' },
-    { id: '6', text: 'a testimonial from Sonke , who is in our IT System Support',image:'https://cdn.discordapp.com/attachments/1096058165172391956/1096075408224563220/IMG-20230413-WA0026.jpg' },
+    { id: '6', text: 'a testimonial from Sonke ,Thank you for the learning experience ',image:'https://cdn.discordapp.com/attachments/1096058165172391956/1096075408224563220/IMG-20230413-WA0026.jpg' },
     { id: '7', text: 'a testimonial from Reginah , who is in our IT System Support',image:'https://cdn.discordapp.com/attachments/1021684832922779648/1021685547468587079/IMG_20220920_085647.jpg' },
     { id: '8', text: 'a testimonial from Thozama , who is in our IT System Support',image:'https://cdn.discordapp.com/attachments/1021684832922779648/1021685470616371240/IMG_20220920_085931.jpg' },
     { id: '9', text: 'a testimonial from Sibongile , who is in our IT System Support' ,image:'https://cdn.discordapp.com/attachments/1021684832922779648/1021685444792041472/IMG_20220920_085751.jpg'},
