6f7ed111b6da0326965014ae09fd6a0456628060
unknown <you@example.com>
2023-05-19 10:01:59 +0200

    backgroup in home and css

--- vite-project/src/pages/Images.jsx
+++ vite-project/src/pages/Images.jsx
@@ -4,12 +4,16 @@ import '../pages/Images.css'
 
 
 const Images = () => {
-  return (
-    <div>
-<img src="https://cdn.discordapp.com/attachments/1096055379567333446/1109010894614175774/IMG_3702.jpg" alt="" />
-            
-    </div>
-  )
+    return (
+        <div>
+            <div class="background">
+                <div class="transbox">
+                    <p>Welcome</p>
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default Images
