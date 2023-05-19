ec85bd3012d623fe177015f49fa77d3658cde8ba
unknown <you@example.com>
2023-05-19 14:00:28 +0200

    add sticky page in Navbar on page and add slide a picture

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -3,32 +3,32 @@ import './Navbar.css'
 
 function Navbar() {
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/"><span className='NavText'>Digital Academy</span></a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/about">AboutUs</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/it_staff">Picture PwD</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/contact">ContactUs</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-    </div>
+
+      <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/"><span className='NavText'>Digital Academy</span></a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/about">AboutUs</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/it_staff">Picture PwD</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/contact">ContactUs</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
   )
 }
 
--- vite-project/src/components/Slider.jsx
+++ vite-project/src/components/Slider.jsx
@@ -1,32 +1,24 @@
-import React from 'react'
-import { useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import '../components/Slider.css'
-import image1 from '../assets/DA_Logo.jpg'
+import './Slider.css'
 
-
-export default function Slider() {
-
-  const [index, setIndex] = useState(0);
-
-  const handleSelect = (selectedIndex) => {
-    setIndex(selectedIndex);
-  };
+function Slider() {
   return (
-    <div>
-
-<Carousel activeIndex={index} onSelect={handleSelect}>
+    <Carousel className='mySlider'>
       <Carousel.Item>
         <img
-          className="d-block"
-          src={image1}
+          className="d-block w-100"
+          src="https://images.unsplash.com/photo-1665686310934-8fab52b3821b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
           alt="First slide"
         />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block"
-          src="holder.js/800x400?text=Second slide&bg=282c34"
+          className="d-block w-100"
+          src="https://images.unsplash.com/photo-1665686306265-c52ee9054479?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
           alt="Second slide"
         />
 
@@ -37,17 +29,20 @@ export default function Slider() {
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block"
-          src="holder.js/800x400?text=Third slide&bg=20232a"
+          className="d-block w-100"
+          src="https://images.unsplash.com/photo-1664575599618-8f6bd76fc670?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
           alt="Third slide"
         />
 
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
-
-
-
-
-    </div>
-  )
+  );
 }
+
+export default Slider;
