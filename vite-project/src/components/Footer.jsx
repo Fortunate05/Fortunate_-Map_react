e91e473c0e79ad8d0a7e7c14e90bfe0a4085bb67
unknown <you@example.com>
2023-05-22 10:53:17 +0200

    fix oversize body width by footer

--- vite-project/src/components/Footer.jsx
+++ vite-project/src/components/Footer.jsx
@@ -2,56 +2,60 @@ import React from 'react'
 import '../components/Footer.css'
 
 const Footer = () => {
-  return (
-    <div>
-<footer>
-        <div class="footer-top">
-            <div class="container">
-                <div class="footer-day-time">
-                    <div class="row">
-                        <div class="col-md-8">
-                            <ul>
-                                <li>Opening Hours: Mon - Friday: 8AM - 5PM</li>
-                                
-                            </ul>
+    return (
+        <div>
+            <footer>
+                <div className="footer-top">
+                    <div className="container">
+                        <div>
+
+
+                            <div className="footer-day-time">
+                                <div className="row">
+                                    <div className="col-md-8">
+                                        <ul>
+                                            <li>Opening Hours: Mon - Friday: 8AM - 5PM</li>
+
+                                        </ul>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="words">
+
+                                    <h4>About us</h4>
+                                    <p>The Digital Academy is dedicated to developing cutting-edge <br />
+                                        digital products in Africa.
+                                        Our mission is to provide chances for <br />
+                                        adolescents through incentive-based learning with
+                                        strategic <br />
+                                        corporate partners who share a similar vision. We were founded <br />
+                                        in response to
+                                        the irritating lack of digital skills on the continent.</p>
+
+                                </div>
+
+                            </div>
                         </div>
-                        
                     </div>
                 </div>
-                <div class="row">
-                    <div class="words">
-                        
-                        <h4>About us</h4>
-                        <p>The Digital Academy is dedicated to developing cutting-edge <br />
-                            digital products in Africa.
-                            Our mission is to provide chances for <br />
-                            adolescents through incentive-based learning with
-                            strategic <br />
-                            corporate partners who share a similar vision. We were founded <br />
-                            in response to
-                            the irritating lack of digital skills on the continent.</p>
+                <div className="footer-bottom">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-sm-5">
+                                <p className="copyrights">Copyright © 2023, Team B
+                                </p>
+                            </div>
 
+                        </div>
                     </div>
-
                 </div>
-            </div>
+            </footer>
+
+
         </div>
-        <div class="footer-bottom">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-5">
-                        <p class="copyrights">Copyright © 2023, Team B
-                        </p>
-                    </div>
-                   
-                </div>
-            </div>
-        </div>
-    </footer>
-
-
-    </div>
-  )
+    )
 }
 
 export default Footer
