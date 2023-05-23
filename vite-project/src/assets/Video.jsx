a2a4af080e5b640d4f8dd79e76be8499cd65155c
unknown <you@example.com>
2023-05-23 10:08:40 +0200

    added loops in video

--- vite-project/src/assets/Video.jsx
+++ vite-project/src/assets/Video.jsx
@@ -6,7 +6,7 @@ const Video = () => {
   return (
     <div>
         
-        <video className='vdo' controls autoPlay muted>
+        <video className='vdo' controls autoPlay muted loop>
          <source src={VideoSchool} type="video/mp4" />
         </video>
     </div>
